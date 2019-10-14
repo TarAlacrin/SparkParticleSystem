7b05eec49d2cb1577013338a759e9aa5b9f644fc
Sam Dresser <42077688+SaganSam@users.noreply.github.com>
2019-10-14 18:55:12 -0400

    Dancing cubes commit
    Expirmenting with a shader for dancing cubes

---
+++ SparkParticleSystem/Assets/DanceBoxes/BufferHandlerDanceBoxes.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BufferHandlerDanceBoxes : MonoBehaviour
+{
+	public const int READ = 1;
+	public const int WRITE = 0;
+
+	ComputeBuffer[] quadDataBuffers = new ComputeBuffer[2];
+	ComputeBuffer quadDataArgBuffer;
+
+	public ComputeShader cShade;
+	public Material material;
+	
+
+	//128//512//1024//2048//4096//8192//16384//32768//65536//131072//262144//524288//1048576
+	public int count = 8;
+	public int cubecount
+	{
+		get
+		{
+			return count * count * count;
+		}
+	}
+
+	public const string _SimKernelName = "CSMain"; //"CSBlockMain";//"CSTwirl";
+	public int simkernal
+	{
+		get
+		{
+			return cShade.FindKernel(_SimKernelName);
+		}
+	}
+
+
+	struct QuadData
+	{
+		Vector3 position;
+		Vector3 normal;
+		float age;
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		quadDataBuffers[READ] = new ComputeBuffer(cubecount * 6, 7 * sizeof(float), ComputeBufferType.Append);
+		quadDataBuffers[WRITE] = new ComputeBuffer(cubecount * 6, 7 * sizeof(float), ComputeBufferType.Append);
+
+		quadDataBuffers[WRITE].SetCounterValue(0);
+		quadDataArgBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments);
+		cShade.SetBuffer(simkernal, "WQuadPositionAndAgeBuffer", quadDataBuffers[WRITE]);
+		cShade.Dispatch(simkernal, count, count, count);
+		Swap(quadDataBuffers);
+		//cShade.SetBuffer(simkernal, "WQuadPositionAndAgeBuffer", quadDataBuffers[WRITE]);
+		//cShade.Dispatch(simkernal, count, count, count);
+
+		//int[] quadargs = new int[] { 0, 1, 0, 0 };
+		//quadDataArgBuffer.SetData(quadargs);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+	}
+
+
+	private void OnRenderObject()
+	{
+		int[] quadargs = GetArgs(quadDataBuffers[READ], quadDataArgBuffer);
+		Debug.Log("Quadargs: " + quadargs[0] + ", " + quadargs[1] + ", " + quadargs[2] + ", " + quadargs[3]);
+		material.SetPass(0); 
+		material.SetBuffer("_Data", quadDataBuffers[READ]);
+		Graphics.DrawProceduralIndirect(MeshTopology.Points, quadDataArgBuffer, 0);
+	}
+
+	private void OnDisable()
+	{
+		Debug.Log("DESTROYED!");
+		quadDataBuffers[READ].Release();
+		quadDataBuffers[WRITE].Release();
+		quadDataArgBuffer.Release();
+	}
+
+
+
+	void Swap(ComputeBuffer[] buffer)
+	{
+		ComputeBuffer tmp = buffer[READ];
+		buffer[READ] = buffer[WRITE];
+		buffer[WRITE] = tmp;
+	}
+
+	int[] GetArgs(ComputeBuffer compOBuffToCheck, ComputeBuffer pargsBuffer)
+	{
+		int[] args = new int[] { 0, 1, 0, 0 };
+		pargsBuffer.SetData(args);
+		ComputeBuffer.CopyCount(compOBuffToCheck, pargsBuffer, 0);
+		pargsBuffer.GetData(args);
+		return args;
+	}
+}
---
+++ SparkParticleSystem/Assets/DanceBoxes/DanceBoxShader.shader
@@ -0,0 +1,117 @@
+Shader "SDnBshades/DanceBox" 
+{
+
+	Properties
+	{
+		_Color("Color", Color) = (0.105,0.0469,1,1)
+		_DeadColor("Dead Color", Color) = (0,0,0,1)
+		_Size("Size", float) = 1
+	}
+	SubShader 
+	{
+		//uses DrawProcedural so tag doesnt really matter
+		Tags { "Queue" = "Transparent" }
+	
+		Pass 
+		{
+		
+		//ZWrite Off 
+		//ZTest LEqual 
+		//Cull Off 
+		Fog { Mode Off }
+    	//Blend SrcAlpha OneMinusSrcAlpha
+		//Blend One One
+     	
+		CGPROGRAM
+		#pragma target 5.0
+
+		#pragma vertex vert
+		#pragma geometry geom
+		#pragma fragment frag
+
+		#include "UnityCG.cginc"
+
+
+		struct inputData {
+			float3 position;
+			float3 normal;
+			float age;
+		};
+
+		StructuredBuffer< inputData> _Data;
+
+		fixed4 _DeadColor;
+
+		fixed4 _Color;
+		float _Size;
+
+		struct v2g 
+		{
+			float4 pos : SV_POSITION;
+			int id : TEXCOORD0;
+			float4 col : COLOR;
+		};
+
+		v2g vert(uint id : SV_VertexID)
+		{
+			v2g OUT;
+			float3 worldPos = _Data[id].position;
+			OUT.pos = mul (UNITY_MATRIX_VP, float4(worldPos,1.0f));
+			OUT.id = id;
+			OUT.col = lerp(_DeadColor, _Color, _Data[id].age*0.1f);
+			//should move tangent and binormal calcs to here.
+			return OUT;
+		}
+		
+		struct g2f {
+		float4 posi : SV_POSITION;
+		float2 uv : TEXCOORD0;
+		float4 colo : COLOR;
+		};
+
+		[maxvertexcount(4)]
+		void geom(point v2g IN[1], inout TriangleStream<g2f> outStream)
+		{
+			g2f OUT;
+			OUT.colo = IN[0].col;
+			float3 up = normalize(lerp(float3(0, 0, 1), float3(0, 1, 0), saturate(ceil(length(abs(_Data[IN[0].id].normal) - float3(0, 1, 0)) )) ));
+			float3 right = normalize( cross(up, _Data[IN[0].id].normal));
+			float4 binormal = mul(UNITY_MATRIX_VP, float4(up, 0)*0.5f*_Size);
+			float4 tangent = mul(UNITY_MATRIX_VP, float4(right, 0)*0.5f*_Size);
+			float4 nrmal = mul(UNITY_MATRIX_VP, float4(_Data[IN[0].id].normal, 0)*0.5f*_Size);
+			
+			OUT.posi = IN[0].pos + nrmal + tangent - binormal; OUT.uv = float2(1,0); outStream.Append(OUT);
+			OUT.posi = IN[0].pos + nrmal + tangent + binormal; OUT.uv = float2(1, 1); outStream.Append(OUT);
+			OUT.posi = IN[0].pos + nrmal - tangent - binormal; OUT.uv = float2(0, 0); outStream.Append(OUT);
+			OUT.posi = IN[0].pos + nrmal - tangent + binormal; OUT.uv = float2(0, 1); outStream.Append(OUT);
+
+			outStream.RestartStrip();
+		}
+
+		float4 frag (g2f IN) : COLOR
+		{
+			return float4(IN.colo.rgb ,IN.colo.a ); 
+		}
+
+		ENDCG
+
+		}
+	}
+
+Fallback Off
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
